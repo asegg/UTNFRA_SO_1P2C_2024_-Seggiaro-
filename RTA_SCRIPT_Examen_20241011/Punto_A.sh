56fa37b5574d8e83c6fc012a04f6be3a7fcd3f87
asegg <aaseggiaro@gmail.com>
2024-10-11 19:39:19 +0000

    Punto 2

--- RTA_SCRIPT_Examen_20241011/Punto_A.sh
+++ RTA_SCRIPT_Examen_20241011/Punto_A.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
 sudo mkdir -p /Examenes-UTN/{Alumno_{1..3}/parcial_{1..3},profesores}
+
---
+++ RTA_SCRIPT_Examen_20241011/Punto_B.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+sudo fdisk -l
+sudo fdisk /dev/sdc
+n
+p
+
+
++1G
+n
+p
+
+
++1G
+n
+p
+
+
++1G
+n
+e
+
+
+
+n
+
++1G
+n
+
++1G
+n
+
++1G
+n
+
++1G
+n
+
++1G
+n
+
++1G
+n
+
+
+w
+sudo mkfs -t ext4/dev/sdc1
+sudo mkfs -t ext4/dev/sdc2
+sudo mkfs -t ext4/dev/sdc3
+sudo mkfs -t ext4/dev/sdc5
+sudo mkfs -t ext4/dev/sdc6
+sudo mkfs -t ext4/dev/sdc7
+sudo mkfs -t ext4/dev/sdc8
+sudo mkfs -t ext4/dev/sdc9
+sudo mkfs -t ext4/dev/sdc10
+sudo mkfs -t ext4/dev/sdc11
+cat /etc/fstab
+echo '/dev/sdc1 /Examenes-UTN/alumno_1/parcial_1 ext4 default 0 0' | sudo tee -a /etc/fstab
+echo '/dev/sdc2 /Examenes-UTN/alumno_1/parcial_2 ext4 default 0 0' | sudo tee -a /etc/fstab
+echo '/dev/sdc3 /Examenes-UTN/alumno_1/parcial_3 ext4 default 0 0' | sudo tee -a /etc/fstab
+echo '/dev/sdc5 /Examenes-UTN/alumno_2/parcial_1 ext4 default 0 0' | sudo tee -a /etc/fstab
+echo '/dev/sdc6 /Examenes-UTN/alumno_2/parcial_2 ext4 default 0 0' | sudo tee -a /etc/fstab
+echo '/dev/sdc7 /Examenes-UTN/alumno_2/parcial_3 ext4 default 0 0' | sudo tee -a /etc/fstab
+echo '/dev/sdc8 /Examenes-UTN/alumno_3/parcial_1 ext4 default 0 0' | sudo tee -a /etc/fstab
+echo '/dev/sdc9 /Examenes-UTN/alumno_3/parcial_2 ext4 default 0 0' | sudo tee -a /etc/fstab
+echo '/dev/sdc10 /Examenes-UTN/alumno_3/parcial_3 ext4 default 0 0' | sudo tee -a /etc/fstab
+echo '/dev/sdc11 /Examenes-UTN/profesores ext4 default 0 0' | sudo tee -a /etc/fstab
+sudo mount -a
+systemctl daemont-reload
+sudo lsblk
+
+
